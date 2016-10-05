51942c801db344c198a015158202ce11a1291b72
Huseyin Koyun <hukoyun@microsoft.com>
2016-10-05 16:19:48 -0700

    server-vars.yml is also updated.

--- scripts/update_theme.sh
+++ scripts/update_theme.sh
@@ -3,6 +3,12 @@
 # Licensed under the MIT license. See LICENSE file on the project webpage for details.
 set -x
 
+sudo cp ../config/server-vars.yml /edx/app/edx_ansible/
+sudo chown edx-ansible:edx-ansible /edx/app/edx_ansible/server-vars.yml
+
+cd /tmp/configuration/playbooks
+sudo ansible-playbook -i localhost, -c local vagrant-fullstack.yml -e@/edx/app/edx_ansible/server-vars.yml -e@/edx/app/edx_ansible/extra-vars.yml -t 'edxapp_cfg'
+
 THEME_PATH=/edx/app/edxapp/themes
 THEME_REPO=https://github.com/microsoft/edx-theme.git
 
@@ -18,9 +24,9 @@ sudo su edxapp -s /bin/bash
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform/
 paver update_assets lms --settings aws
-sudo su openedxuser -s /bin/bash
+exit
 sudo /edx/bin/supervisorctl restart edxapp:
-
+echo "Huseyin -> Finished applying Microsoft Stanford Theming." 
 
 
 
