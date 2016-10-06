80747d707830e5e2e8a273430da6d8f7173ca8e3
Huseyin Koyun <hukoyun@microsoft.com>
2016-10-06 16:16:22 -0700

    sudo

--- scripts/update_theme.sh
+++ scripts/update_theme.sh
@@ -7,7 +7,7 @@ sudo cp server-vars.yml /edx/app/edx_ansible/
 sudo chown edx-ansible:edx-ansible /edx/app/edx_ansible/server-vars.yml
 
 cd /tmp/configuration/playbooks
-sudo ansible-playbook -i localhost, -c local vagrant-fullstack.yml -e@/edx/app/edx_ansible/server-vars.yml -e@/edx/app/edx_ansible/extra-vars.yml
+sudo ansible-playbook -i localhost, -c local vagrant-fullstack.yml -e@/edx/app/edx_ansible/server-vars.yml -e@/edx/app/edx_ansible/extra-vars.yml -t 'edxapp_cfg'
 
 THEME_PATH=/edx/app/edxapp/themes
 THEME_REPO=https://github.com/microsoft/edx-theme.git
@@ -15,11 +15,14 @@ THEME_REPO=https://github.com/microsoft/edx-theme.git
 if [[ -d $THEME_PATH ]]; then
   sudo rm -fr $THEME_PATH
 fi
-
 sudo mkdir -p $THEME_PATH
 
 cd $THEME_PATH
 sudo git clone -b pilot $THEME_REPO default
+
+if [[ -d /tmp/sass-cache ]]; then
+  sudo rm -fr /tmp/sass-cache
+fi
 
 sudo chown -R edxapp:edxapp $THEME_PATH
 sudo su edxapp -s /bin/bash -c "source /edx/app/edxapp/edxapp_env;cd /edx/app/edxapp/edx-platform/;paver update_assets cms --settings aws;paver update_assets lms --settings aws"
