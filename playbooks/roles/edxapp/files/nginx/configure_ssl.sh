e4cb6e6fdd557efe2350aba0d08736615359555e
“Brian <btelnes@microsoft.com>
2016-10-21 22:56:27 +0000

    Enable SSL for LMS/CMS based on environment settings

--- playbooks/roles/edxapp/files/nginx/configure_ssl.sh
+++ playbooks/roles/edxapp/files/nginx/configure_ssl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Copyright (c) Microsoft Corporation. All Rights Reserved.
 # Licensed under the MIT license. See LICENSE file on the project webpage for details.
+echo "Port: $2"
+
 echo "Copy SSL cert from secrets folder"
 mkdir -p /etc/nginx/ssl
 cp $1/nginx/cert/openedx.microsoft.com.crt /etc/nginx/ssl/openedx.microsoft.com.crt
@@ -8,16 +10,28 @@ cp $1/nginx/cert/openedx.microsoft.com.key /etc/nginx/ssl/openedx.microsoft.com.
 
 echo "Preserve the original lms file"
 if [ ! -f /edx/app/nginx/sites-available/lms-original ]; then
-  mv /edx/app/nginx/sites-available/lms /edx/app/nginx/sites-available/lms-original
+  cp /edx/app/nginx/sites-available/lms /edx/app/nginx/sites-available/lms-original
 fi
 
 echo "Copy the updated lms file" # symlinked: /etc/nginx/sites-enabled/lms -> /edx/app/nginx/sites-available/lms
-cp sites-available/lms /edx/app/nginx/sites-available/lms
+if [[ $2 -eq 443 ]]; then
+  echo "Enabling LMS using SSL"
+  cp sites-available/SSL/lms /edx/app/nginx/sites-available/lms
+else
+  echo "Enabling LMS without SSL"
+  cp /edx/app/nginx/sites-available/lms-original /edx/app/nginx/sites-available/lms
+fi
 
 echo "Preserve the original cms file"
 if [ ! -f /edx/app/nginx/sites-available/cms-original ]; then
-  mv /edx/app/nginx/sites-available/cms /edx/app/nginx/sites-available/cms-original
+  cp /edx/app/nginx/sites-available/cms /edx/app/nginx/sites-available/cms-original
 fi
 
 echo "Copy the updated cms file" # symlinked: /etc/nginx/sites-enabled/cms -> /edx/app/nginx/sites-available/cms
-cp sites-available/cms /edx/app/nginx/sites-available/cms
+if [[ $2 -eq 443 ]]; then
+  echo "Enabling CMS using SSL"
+  cp sites-available/SSL/cms /edx/app/nginx/sites-available/cms
+else
+  echo "Enabling CMS without SSL"
+  cp /edx/app/nginx/sites-available/cms-original /edx/app/nginx/sites-available/cms
+fi
