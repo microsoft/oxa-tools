0d0623b90916ccc71cb2a8c8d87631a98bae4d21
localstepdo <stephendolenc@gmail.com>
2016-11-02 15:43:40 -0700

    prevent blocking dialogs during setup

--- templates/stamp/utilities.sh
+++ templates/stamp/utilities.sh
@@ -65,6 +65,13 @@ configure_datadisks()
 {
 	# Stripe all of the data 
 	log "Formatting and configuring the data disks"
-	
+
+    # vm-disk-utils-0.1 can install mdadm which also installs postfix.
+    # The postfix installation cannot be made silent using the same techniques
+    # that keep the mdadm quiet: -y AND DEBIAN_FRONTEND=noninteractive.
+    # Therefore, we'll install postfix early with the "No configuration" option.
+    echo "postfix postfix/main_mailer_type select No configuration" | sudo debconf-set-selections
+    sudo apt-get install -y postfix
+
 	bash ./vm-disk-utils-0.1.sh -b $DATA_DISKS -s
 }
