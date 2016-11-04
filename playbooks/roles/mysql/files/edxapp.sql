08797c7d8308cc259dd684f84e6ccdcdd5735cd0
ms-hukoyun <hukoyun@microsoft.com>
2016-11-04 11:58:40 -0700

    Adding unique constraint to the email field of auth_user so that multiple accounts with same e-mail cannot be created. This was default behaviour in Python 2.7.3 and Django 1.4 but it is changed in Python 2.7.10 and Django 1.8.7

--- playbooks/roles/mysql/files/edxapp.sql
+++ playbooks/roles/mysql/files/edxapp.sql
@@ -2,4 +2,5 @@ UPDATE auth_user SET is_active=False where username="staff";
 UPDATE auth_user SET is_active=False where username="honor";
 UPDATE auth_user SET is_active=False where username="audit";
 UPDATE auth_user SET is_active=False where username="verified";
+ALTER TABLE auth_user ADD UNIQUE (email);
 
