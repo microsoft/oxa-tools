ec5857ba0fcbdf8ba3616fcde363c0f7bef4f5b0
ms-hukoyun <hukoyun@microsoft.com>
2016-11-04 20:13:22 -0700

    Fixing the theming automation. Existing task unfortunatelty doesnot work. I added update_theme.sh and updated the task to call this script file.

---
+++ playbooks/roles/edxapp/files/update_theme.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All Rights Reserved.
+# Licensed under the MIT license. See LICENSE file on the project webpage for details.
+
+set -x
+
+# Delete the folder /tmp/sass-cache if exists. This is the temp folder for compiled assets
+if [[ -d /tmp/sass-cache ]]; then
+  sudo rm -fr /tmp/sass-cache
+fi
+
+# Compile LMS assets and then restart the services so that changes take effect
+sudo su edxapp -s /bin/bash -c "source /edx/app/edxapp/edxapp_env;cd /edx/app/edxapp/edx-platform/;paver update_assets lms --settings aws"
+sudo /edx/bin/supervisorctl restart edxapp:
+
+# Compile LMS assets and then restart the services so that changes take effect.
+# We do the same operation twice here since mostly it doesnot work in the first run. This is a workaround.
+sudo su edxapp -s /bin/bash -c "source /edx/app/edxapp/edxapp_env;cd /edx/app/edxapp/edx-platform/;paver update_assets lms --settings aws"
+sudo /edx/bin/supervisorctl restart edxapp:
+
