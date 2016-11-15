1b5613fac71e8d038ad2293d74afb85d40ec3066
Elton.C <eltonc@microsoft.com>
2016-11-15 05:37:24 -0800

    Fixing LMS/Forums startup failures due to MongoDB authentication

--- templates/stamp/mongodb-ubuntu-install.sh
+++ templates/stamp/mongodb-ubuntu-install.sh
@@ -345,6 +345,8 @@ configure_db_users()
     fi
 
     # this command will re-create (remove if necessary) the admin user account
+    # OpenEdX assumes the authentication database is already operational. Otherwise it fails. Therefore, setting authentication against the expected/default database OpenEdx expects
+    mongo edxapp --host 127.0.0.1 --eval "db.dropUser('${ADMIN_USER_NAME}'); db.createUser({user: '${ADMIN_USER_NAME}', pwd: '${ADMIN_USER_PASSWORD}', roles:[{ role: 'userAdminAnyDatabase', db: 'admin' }, { role: 'clusterAdmin', db: 'admin' }, { role: 'readWriteAnyDatabase', db: 'admin' }, { role: 'dbAdminAnyDatabase', db: 'admin' } ]})"
     mongo master --host 127.0.0.1 --eval "db.dropUser('${ADMIN_USER_NAME}'); db.createUser({user: '${ADMIN_USER_NAME}', pwd: '${ADMIN_USER_PASSWORD}', roles:[{ role: 'userAdminAnyDatabase', db: 'admin' }, { role: 'clusterAdmin', db: 'admin' }, { role: 'readWriteAnyDatabase', db: 'admin' }, { role: 'dbAdminAnyDatabase', db: 'admin' } ]})"
 }
 
