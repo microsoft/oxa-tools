bb42c8541e979819a83695f7f95757134115afcc
Christy Henriksson <chenriks@microsoft.com>
2016-11-22 21:36:42 -0800

    Adding back oxa playbook run for mysql, but against localhost

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -265,7 +265,9 @@ update_stamp_jb() {
   # oxa playbooks - mongo (enable when customized) and mysql
   #$ANSIBLE_PLAYBOOK -i ${CLUSTERNAME}mongo1, $OXA_SSH_ARGS -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "mongo"
   #exit_on_error "Execution of OXA Mongo playbook failed"
-  #exit_on_error "Execution of OXA MySQL playbook failed"
+
+  $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "mysql"
+  exit_on_error "Execution of OXA MySQL playbook failed"
 }
 
 update_stamp_vmss() {
