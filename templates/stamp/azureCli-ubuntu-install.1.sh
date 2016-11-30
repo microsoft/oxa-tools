bd5a612706198994105b500f44653d74d151b2c0
localstepdo <stephendolenc@gmail.com>
2016-11-30 15:00:05 -0800

    wrapper to execute new playbook

--- templates/stamp/azureCli-ubuntu-install.1.sh
+++ templates/stamp/azureCli-ubuntu-install.1.sh
@@ -12,3 +12,17 @@ source ./utilities.sh
   install-mongodb-shell
   install-mysql-client
   install-json-processor
+
+
+OXA_PATH=/oxa
+DEPLOYMENT_ENV=bvt
+ADMIN_USER=lexoxaadmin
+ANSIBLE_PLAYBOOK=ansible-playbook
+
+OXA_TOOLS_PATH=$OXA_PATH/oxa-tools
+OXA_PLAYBOOK=$OXA_TOOLS_PATH/playbooks/oxa_configuration.yml
+OXA_TOOLS_CONFIG_PATH=$OXA_PATH/oxa-tools-config
+OXA_ENV_FILE=$OXA_TOOLS_CONFIG_PATH/env/$DEPLOYMENT_ENV/$DEPLOYMENT_ENV.sh
+OXA_PLAYBOOK_ARGS="-e oxa_tools_path=$OXA_TOOLS_PATH -e oxa_env_file=$OXA_ENV_FILE -e admin_user=$ADMIN_USER"
+
+$ANSIBLE_PLAYBOOK -i localhost, -c local $OXA_PLAYBOOK_ARGS -e $OXA_PLAYBOOK --tags "jumpbox"
