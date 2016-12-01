ce8d55f30079192cb5442c5120f21605407ef5b1
localstepdo <stephendolenc@gmail.com>
2016-11-30 16:17:20 -0800

    fixing playbook command

--- templates/stamp/azureCli-ubuntu-install.1.sh
+++ templates/stamp/azureCli-ubuntu-install.1.sh
@@ -26,12 +26,5 @@ OXA_TOOLS_CONFIG_PATH=$OXA_PATH/oxa-tools-config
 OXA_ENV_FILE=$OXA_TOOLS_CONFIG_PATH/env/$DEPLOYMENT_ENV/$DEPLOYMENT_ENV.sh
 OXA_PLAYBOOK_ARGS="-e oxa_tools_path=$OXA_TOOLS_PATH -e oxa_env_file=$OXA_ENV_FILE -e admin_user=$ADMIN_USER"
 
-    # aggregate edx configuration with deployment environment expansion
-    # warning: beware of yaml variable dependencies due to order of aggregation
-    echo "---" > $OXA_PLAYBOOK_CONFIG
-    for config in $OXA_TOOLS_PATH/config/$TEMPLATE_TYPE/*.yml $OXA_TOOLS_PATH/config/*.yml; do
-        sed -e "s/%%\([^%]*\)%%/$\{\\1\}/g" -e "s/^---.*$//g" $config | envsubst >> $OXA_PLAYBOOK_CONFIG
-    done
-
-echo $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "jumpbox"
-$ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "jumpbox"
+echo $ANSIBLE_PLAYBOOK -i localhost, -c local $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "jumpbox"
+$ANSIBLE_PLAYBOOK -i localhost, -c local $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "jumpbox"
