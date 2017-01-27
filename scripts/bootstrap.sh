279d619e7b4e332d9a7acbf18ee7f7e20c30415a
“Brian <btelnes@microsoft.com>
2017-01-27 19:33:04 +0000

    Changes for devstack deployment

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -15,8 +15,21 @@ TARGET_FILE=""
 PROGRESS_FILE=""
 
 display_usage() {
-  echo "Usage: $0 -a|--access_token {access token} -v|--version {oxa-tools-config version} [-r|--role {jb|vmss|mongo|mysql|edxapp|fullstack}] [-e|--environment {dev|bvt|int|prod}] [--cron]"
+  echo "Usage: $0 -a|--access_token {access token} -v|--version {oxa-tools-config version} [-r|--role {jb|vmss|mongo|mysql|edxapp|fullstack|devstack}] [-e|--environment {dev|bvt|int|prod}] [--cron]"
   exit 1
+}
+
+is_valid_arg() {
+  local list="$1"
+  local arg="$2"
+
+  if [[ $list =~ (^|[[:space:]])"$arg"($|[[:space:]]) ]] ; then
+    result=0
+  else
+    result=1
+  fi
+
+  return $result
 }
 
 is_valid_arg() {
@@ -38,7 +51,7 @@ parse_args() {
     case "$1" in
       -r|--role)
         EDX_ROLE="${2,,}" # convert to lowercase
-        if is_valid_arg "jb vmss mongo mysql edxapp fullstack" $EDX_ROLE; then
+        if is_valid_arg "jb vmss mongo mysql edxapp fullstack devstack" $EDX_ROLE; then
           shift # past argument
         else
           echo "Invalid role specified\n"
@@ -92,9 +105,9 @@ sync_repo() {
   if [ "$#" -lt 3 ]; then
     echo "sync_repo: invalid number of arguments" && exit 1
   fi
-  
+
   # todo: scorch support?
-  
+
   if [[ ! -d $REPO_PATH ]]; then
     mkdir -p $REPO_PATH
     git clone ${REPO_URL/github/$REPO_TOKEN@github} $REPO_PATH
@@ -147,7 +160,7 @@ get_bootstrap_status()
             # It is expected that the client tools are already installed
             #echo "Testing connection to edxapp database on '${MYSQL_MASTER_IP}'"
             AUTH_USER_COUNT=`mysql -u $MYSQL_ADMIN_USER -p$MYSQL_ADMIN_PASSWORD -h $MYSQL_MASTER_IP -s -N -e "use edxapp; select count(*) from auth_user;"`
-            if [[ $? -ne 0 ]]; 
+            if [[ $? -ne 0 ]];
             then
                 #echo "Connection test failed. Keeping holding pattern for VMSS bootstrap"
                 # The crumb doesn't exist:: we need to execute boostrap, but we have unmet dependency (wait)
@@ -177,20 +190,20 @@ setup_overrides()
 ##
 ## Role-independent OXA environment bootstrap
 ##
-setup() 
+setup()
 {
     apt-get -y update
     apt-get -y install git
-  
+
     # sync the private repository
     sync_repo $OXA_TOOLS_CONFIG_REPO $OXA_TOOLS_CONFIG_VERSION $OXA_TOOLS_CONFIG_PATH $ACCESS_TOKEN
-  
+
     # populate the deployment environment
     source $OXA_ENV_FILE
     setup_overrides
 
     export $(sed -e 's/#.*$//' $OXA_ENV_FILE | cut -d= -f1)
-  
+
     # deployment environment overrides for debugging
     OXA_ENV_OVERRIDE_FILE="$BOOTSTRAP_HOME/overrides.sh"
     if [[ -f $OXA_ENV_OVERRIDE_FILE ]]; then
@@ -201,11 +214,11 @@ setup()
     export $(sed -e 's/#.*$//' $OXA_ENV_OVERRIDE_FILE | cut -d= -f1)
     export ANSIBLE_REPO=$CONFIGURATION_REPO
     export ANSIBLE_VERSION=$CONFIGURATION_VERSION
-  
+
     # sync public repositories
     sync_repo $OXA_TOOLS_REPO $OXA_TOOLS_VERSION $OXA_TOOLS_PATH
     sync_repo $CONFIGURATION_REPO $CONFIGURATION_VERSION $CONFIGURATION_PATH
-  
+
     # run edx bootstrap and install requirements
     cd $CONFIGURATION_PATH
     ANSIBLE_BOOTSTRAP_SCRIPT=util/install/ansible-bootstrap.sh
@@ -224,10 +237,10 @@ setup()
 
     pip install -r requirements.txt
     exit_on_error "Failed pip-installing EdX requirements"
-  
+
     # fix OXA environment ownership
     chown -R $ADMIN_USER:$ADMIN_USER $OXA_PATH
-  
+
     # aggregate edx configuration with deployment environment expansion
     # warning: beware of yaml variable dependencies due to order of aggregation
     echo "---" > $OXA_PLAYBOOK_CONFIG
@@ -253,7 +266,7 @@ exit_on_error()
     fi
 }
 
-update_stamp_jb() {    
+update_stamp_jb() {
   # edx playbooks - mysql and memcached
   $ANSIBLE_PLAYBOOK -i 10.0.0.16, $OXA_SSH_ARGS -e@$OXA_PLAYBOOK_CONFIG edx_mysql.yml
   exit_on_error "Execution of edX MySQL playbook failed"
@@ -261,7 +274,7 @@ update_stamp_jb() {
   # minimize tags? "install:base,install:system-requirements,install:configuration,install:app-requirements,install:code"
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG edx_sandbox.yml -e "migrate_db=yes" --tags "edxapp-sandbox,install,migrate"
   exit_on_error "Execution of edX MySQL migrations failed"
-  
+
   # oxa playbooks - mongo (enable when customized) and mysql
   #$ANSIBLE_PLAYBOOK -i ${CLUSTERNAME}mongo1, $OXA_SSH_ARGS -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "mongo"
   #exit_on_error "Execution of OXA Mongo playbook failed"
@@ -274,7 +287,7 @@ update_stamp_vmss() {
   # edx playbooks - sandbox with remote mongo/mysql
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG edx_sandbox.yml -e "migrate_db=no" --skip-tags=demo_course
   exit_on_error "Execution of edX sandbox playbook failed"
-  
+
   # oxa playbooks
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "edxapp"
   exit_on_error "Execution of OXA edxapp playbook failed"
@@ -284,7 +297,7 @@ update_scalable_mongo() {
   # edx playbooks - mongo
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG edx_mongo.yml
   exit_on_error "Execution of edX Mongo playbook failed"
-  
+
   # oxa playbooks - mongo (enable when customized)
   #$ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "mongo"
   #exit_on_error "Execution of OXA Mongo playbook failed"
@@ -293,11 +306,11 @@ update_scalable_mongo() {
 update_scalable_mysql() {
   # edx playbooks - mysql and memcached
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG edx_mysql.yml
-  exit_on_error "Execution of edX MySQL playbook failed"  
+  exit_on_error "Execution of edX MySQL playbook failed"
   # minimize tags? "install:base,install:system-requirements,install:configuration,install:app-requirements,install:code"
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG edx_sandbox.yml -e "migrate_db=yes" --tags "edxapp-sandbox,install,migrate"
   exit_on_error "Execution of edX MySQL migrations failed"
-  
+
   # oxa playbooks - mysql
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "mysql"
   exit_on_error "Execution of OXA MySQL playbook failed"
@@ -310,6 +323,27 @@ update_fullstack() {
 
   # oxa playbooks - all (single VM)
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK
+  exit_on_error "Execution of OXA playbook failed"
+}
+
+update_devstack() {
+  # create required vagrant user account to avoid fatal error
+  sudo adduser -m -p $VAGRANT_USER_PASSWORD vagrant
+
+  # create some required directories to avoid fatal error
+  sudo mkdir -p /edx/app/ecomworker
+  sudo mkdir -p /home/vagrant/share_x11
+
+  # create empty .bashrc file to avoid fatal error
+  sudo touch /home/vagrant/.bashrc
+
+  # edx playbooks - devstack (single VM)
+  # Skip ecommerce for now since it isn't used and requires debugging
+  $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG vagrant-devstack.yml --skip-tags="ecommerce,ecomworker"
+  exit_on_error "Execution of edX devstack playbook failed"
+
+  # oxa playbooks - all (single VM)
+  $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK -e "edxrole=$EDX_ROLE"
   exit_on_error "Execution of OXA playbook failed"
 }
 
@@ -423,6 +457,9 @@ case "$EDX_ROLE" in
     ;;
   fullstack)
     update_fullstack
+    ;;
+  devstack)
+    update_devstack
     ;;
   *)
     display_usage
