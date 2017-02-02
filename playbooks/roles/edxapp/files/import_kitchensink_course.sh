08d605918ac4bb912994c273f2f6e8023dca8c20
ms-hukoyun <hukoyun@microsoft.com>
2017-02-02 12:10:24 -0800

    Adding kitchensink import automation on behalf of Ashish.

---
+++ playbooks/roles/edxapp/files/import_kitchensink_course.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All Rights Reserved.
+# Licensed under the MIT license. See LICENSE file on the project webpage for details.
+
+set -x
+
+# Remove if kitchen sink course folder exists
+if [[ -d /tmp/ks_source ]]; then
+  sudo rm -fr /tmp/ks_source
+fi
+
+# Create folder for kitchen sink course
+sudo mkdir /tmp/ks_source
+cd /tmp
+
+# Download kitchen sink course from github to folder /tmp/ks_source 
+sudo git clone https://github.com/Microsoft/oxa_kitchen_sink.git ks_source
+
+sudo chown -R edxapp:www-data /tmp/ks_source
+
+# Go to edx-platform folder for importing
+cd /edx/app/edxapp/edx-platform/
+
+# Import kitchen sink course into the platform
+sudo -u www-data /edx/bin/python.edxapp ./manage.py cms --settings=aws import /edx/var/edxapp/data /tmp/ks_source
+
+
+
+
+
