a527fd460b0cdf84b90c53de6a2079ce6c956458
satyarapelly <satya.rapelly@gmail.com>
2017-02-20 15:16:03 -0800

    Fic Deployment

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -276,8 +276,8 @@ update_stamp_jb() {
   exit_on_error "Execution of edX MySQL migrations failed"
 
   # oxa playbooks - mongo (enable when customized) and mysql
-  #$ANSIBLE_PLAYBOOK -i ${CLUSTERNAME}mongo1, $OXA_SSH_ARGS -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "mongo"
-  #exit_on_error "Execution of OXA Mongo playbook failed"
+  $ANSIBLE_PLAYBOOK -i ${CLUSTERNAME}mongo1, $OXA_SSH_ARGS -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "mongo"
+  exit_on_error "Execution of OXA Mongo playbook failed"
 
   $ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG $OXA_PLAYBOOK_ARGS $OXA_PLAYBOOK --tags "mysql"
   exit_on_error "Execution of OXA MySQL playbook failed"
