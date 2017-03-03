fcaaa88ec8e8a62670b8b68ae2785fc6d9e86987
ms-hukoyun <hukoyun@microsoft.com>
2017-03-03 12:40:05 -0800

    Adding history table data migration script which will be used from dogwood to euc/fic database migration and csmh history table separation for performance reason.

---
+++ migrations/database/migrate_scripts/csmh_table_migrate.sh
@@ -0,0 +1,20 @@
+# Original script is here https://github.com/edx/configuration/blob/master/util/csmh-extended/migrate-same-database-instance.sh
+
+MINID=0
+MAXID=1000000
+STEP=10000
+MIGRATE_USER=''
+PASSWORD=''
+HOST=''
+
+for ((i=$MINID; i<=$MAXID; i+=$STEP)); do
+echo -n "$i";
+mysql -u $MIGRATE_USER -p$PASSWORD -h $HOST edxapp<<EOF
+INSERT INTO edxapp_csmh.coursewarehistoryextended_studentmodulehistoryextended (id, version, created, state, grade, max_grade, student_module_id)
+  SELECT id, version, created, state, grade, max_grade, student_module_id
+  FROM edxapp.courseware_studentmodulehistory
+  WHERE id BETWEEN $i AND $(($i+$STEP-1));
+EOF
+echo '.';
+sleep 2
+done
