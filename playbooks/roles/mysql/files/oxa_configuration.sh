6004c30bad39415329ed2fa7d643cb7555b29355
ms-hukoyun <hukoyun@microsoft.com>
2017-03-10 11:30:17 -0800

    Updated the AAD automation T-SQL for new fields of Ficus: - visible : true (1) - provider_slug: 'azuread-oauth2' (There is a bug in Ficus and it uses   provider_slug instead of backend_name so we must give exactly same name   to slug as a workaround to this bug.

--- playbooks/roles/mysql/files/oxa_configuration.sh
+++ playbooks/roles/mysql/files/oxa_configuration.sh
@@ -7,9 +7,9 @@ if [ "$7" == "true" ] || [ "$7" == "True" ]; then
 echo "BEGIN; " > edxapp_aad.sql
 echo "DELETE FROM third_party_auth_oauth2providerconfig where backend_name='azuread-oauth2';" >> edxapp_aad.sql
 echo "INSERT INTO third_party_auth_oauth2providerconfig " >> edxapp_aad.sql
-echo "(change_date,enabled,icon_class,name,secondary,skip_registration_form,skip_email_verification,backend_name,third_party_auth_oauth2providerconfig.key,secret,other_settings,icon_image,site_id) " >> edxapp_aad.sql
+echo "(change_date,enabled,icon_class,name,secondary,skip_registration_form,skip_email_verification,backend_name,third_party_auth_oauth2providerconfig.key,secret,other_settings,icon_image,site_id,visible,provider_slug) " >> edxapp_aad.sql
 echo " VALUES " >> edxapp_aad.sql
-echo "(NOW(),1,'fa-sign-in','$4',0,1,1,'azuread-oauth2','$5','$6','','',1);" >> edxapp_aad.sql
+echo "(NOW(),1,'fa-sign-in','$4',0,1,1,'azuread-oauth2','$5','$6','','',1,1,'azuread-oauth2');" >> edxapp_aad.sql
 echo " COMMIT; " >> edxapp_aad.sql
 
 mysql edxapp --host=$1 --user=$2 --password=$3  < edxapp_aad.sql
