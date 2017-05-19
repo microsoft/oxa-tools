81692dd3ad9cd0905dd38d70222c90800eee1736
Elton Carr, II <eltonc@microsoft.com>
2017-05-19 12:10:42 -0700

    Edxapp Super User Fix - explicitly specify the application super user account details

--- playbooks/roles/edxapp/files/create_superuser.sh
+++ playbooks/roles/edxapp/files/create_superuser.sh
@@ -4,13 +4,20 @@
 
 set -x
 
+# collect the input parameters for easy identification/reference
+edxapp_superuser_password=$1
+edxapp_superuser_emailaddress=$2
+edxapp_superuser_username=$3
+
+# When creating the user account that will be used as the application Super User, the username is derived from the email address
+# if it is not explicitly specified. When derived and edxapp_superuser_name!=the derived user name, setting the super user status fails quietly
+# Fix: explicitly specify the username & emailaddress instead of depending on the default
+
 # Go to edx-platform folder
 cd /edx/app/edxapp/edx-platform
 
 # Create staff user 
-sudo -u www-data /edx/bin/python.edxapp ./manage.py lms --settings=aws create_user -s -p $1 -e $2
+sudo -u www-data /edx/bin/python.edxapp ./manage.py lms --settings=aws create_user -s --password="${edxapp_superuser_password}" --email="${edxapp_superuser_emailaddress}" --username="${edxapp_superuser_username}"
 
 # Make superuser and make active
-echo "from django.contrib.auth.models import User; me = User.objects.get(username='"$3"'); me.is_superuser=True; me.is_active=True; me.save()" | sudo -u www-data /edx/bin/python.edxapp ./manage.py lms --settings=aws shell
-
-
+echo "from django.contrib.auth.models import User; me = User.objects.get(username='"${edxapp_superuser_username}"'); me.is_superuser=True; me.is_active=True; me.save();" | sudo -u www-data /edx/bin/python.edxapp ./manage.py lms --settings=aws shell
