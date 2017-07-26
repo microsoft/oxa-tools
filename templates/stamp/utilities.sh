0ef955b01c01ac05b8b515cbc53f629114c24341
root <stepdo@microsoft.com>
2017-07-26 00:53:57 +0000

    dont use sudo while trying to install sudo

--- templates/stamp/utilities.sh
+++ templates/stamp/utilities.sh
@@ -312,7 +312,7 @@ install-mysql-utilities()
 apt-wrapper()
 {
     log "$1 package(s)..."
-    sudo apt-get $1 -y -qq --fix-missing
+    apt $1 -y -qq --fix-missing
 }
 
 retry-command()
@@ -330,7 +330,7 @@ retry-command()
             apt-wrapper "update"
             apt-wrapper "install -f"
             apt-wrapper "upgrade -f"
-            sudo dpkg --configure -a
+            dpkg --configure -a
         fi
 
         log "STARTING ${message}..."
