f82e2b2868b4c87361432535535f2afc908ad9a9
localstepdo <stepdo@microsoft.com>
2017-09-19 19:27:32 -0700

    create theming directory before big playbook

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -426,6 +426,20 @@ edx_installation_playbook()
   systemctl >/dev/null 2>&1
   exit_on_error "Single VM instance of EDX has a hard requirement on systemd and its systemctl functionality"
 
+  EDXAPP_COMPREHENSIVE_THEME_DIR=`echo EDXAPP_COMPREHENSIVE_THEME_DIRS | tr -d [ | tr -d ] | tr -d " "`
+  # When the comprehensive theming dirs is specified, edxapp:migrate task fails with :  ImproperlyConfigured: COMPREHENSIVE_THEME_DIRS
+  # As an interim mitigation, create the folder if the path specified is not under the edx-platform directory (where the default themes directory is)
+  if [[ -n "${EDXAPP_COMPREHENSIVE_THEME_DIR}" ]] && [[ ! -d "${EDXAPP_COMPREHENSIVE_THEME_DIR}" ]] ; then
+    # now check if the path specified is within the default edx-platform/themes directory
+    if [[ "${EDXAPP_COMPREHENSIVE_THEME_DIR}" == *"${EDX_PLATFORM_PUBLIC_GITHUB_PROJECTNAME}"* ]] ; then
+      log "'${EDXAPP_COMPREHENSIVE_THEME_DIR}' falls under the default theme directory. Skipping creation since the edx-platform clone will create it."
+    else
+      log "Creating comprehensive themeing directory at ${EDXAPP_COMPREHENSIVE_THEME_DIR}"
+      mkdir -p "${EDXAPP_COMPREHENSIVE_THEME_DIR}"
+      chown -R edxapp:edxapp "${EDXAPP_COMPREHENSIVE_THEME_DIR}"
+    fi
+  fi
+
   command="$ANSIBLE_PLAYBOOK -i localhost, -c local -e@$OXA_PLAYBOOK_CONFIG vagrant-${EDX_ROLE}.yml"
 
   # We've been experiencing intermittent failures on ficus. Simply retrying
