c5c0c75c699f68ef700b70861c86ac7fa659ffe5
Elton Carr, II <eltonc@microsoft.com>
2017-11-03 13:38:49 -0700

    Remove hardcoded themes directory reference (honoring user configuration from cloud configs)

--- playbooks/roles/edxapp/files/update_theme.sh
+++ playbooks/roles/edxapp/files/update_theme.sh
@@ -7,31 +7,39 @@ set -ex
 THEME_BRANCH=$1
 EDX_THEME_REPO=$2
 ENVIRONMENT=$3
+THEMES_DIRECTORY=$4
 
-dir_edxapp=/edx/app/edxapp
-dir_themes=${dir_edxapp}/themes
+themes_parent_directory="$(dirname "${THEMES_DIRECTORY}")"
+edxapp_directory="/edx/app/edxapp"
 
-# Remove if themes folder exists
-if [[ -d $dir_themes ]] ; then
-    sudo rm -fr $dir_themes
+# Check if the base directory exists.
+# This is user specified now
+if [[ ! -d $themes_parent_directory ]] ; then
+    sudo mkdir -p $themes_parent_directory
 fi
 
-cd $dir_edxapp
+# Remove the themes folder if it exists
+if [[ -d $THEMES_DIRECTORY ]] ; then
+    sudo rm -fr $THEMES_DIRECTORY
+fi
 
 # Download comprehensive theming from github to folder $dir_themes/comprehensive 
-sudo git clone $EDX_THEME_REPO $dir_themes -b $THEME_BRANCH
+sudo git clone $EDX_THEME_REPO $THEMES_DIRECTORY -b $THEME_BRANCH
 
 # todo:100627 this doesn't work on onebox installations (fullstack and devstack) which don't use oxa-tools-config
 # Generalizing - we only need to do this when there are images that should be copied.
-if [[ -f "/oxa/oxa-tools-config/env/${ENVIRONMENT}/*.png" ]]; then
-    for i in `ls -d1 $dir_themes/*/lms/static/images`; do
+custom_image_count=`ls /oxa/oxa-tools-config/env/${ENVIRONMENT}/*.png 2>/dev/null | wc -w`
+
+if (( $(echo "$custom_image_count > 0" | bc -l) )); then
+    for i in `ls -d1 $THEMES_DIRECTORY/*/lms/static/images`; do
         sudo cp /oxa/oxa-tools-config/env/$ENVIRONMENT/*.png $i;
     done
 fi
-    
-sudo chown -R edxapp:edxapp $dir_themes
-sudo chmod -R u+rw $dir_themes
+
+# set appropriate permissions on the new theming folder
+sudo chown -R edxapp:edxapp $THEMES_DIRECTORY
+sudo chmod -R u+rw $THEMES_DIRECTORY
 
 # Compile LMS assets and then restart the services so that changes take effect
-sudo su edxapp -s /bin/bash -c "source $dir_edxapp/edxapp_env;cd $dir_edxapp/edx-platform/;paver update_assets lms --settings aws"
+sudo su edxapp -s /bin/bash -c "source $edxapp_directory/edxapp_env;cd $edxapp_directory/edx-platform/;paver update_assets lms --settings aws"
 sudo /edx/bin/supervisorctl restart edxapp:
