ad8fd8afd24cb157dff6ee70264e1aae9d22d6ba
Stephen Dolenc <stepdo@microsoft.com>
2018-01-18 17:18:41 -0800

    cleanup conditional around kitchen sink course
    (cherry picked from commit 8218671d5f0e84004916cfd62d1496735361c839)

--- playbooks/roles/edxapp/files/import_kitchensink_course.sh
+++ playbooks/roles/edxapp/files/import_kitchensink_course.sh
@@ -2,32 +2,27 @@
 # Copyright (c) Microsoft Corporation. All Rights Reserved.
 # Licensed under the MIT license. See LICENSE file on the project webpage for details.
 
-set -x
+set -ex
 
-# If set to true import Kitchen Sink Course, otherwise do nothing
-# EDXAPP_IMPORT_KITCHENSINK_COURSE: true
-if [ "$1" == "true" ] || [ "$1" == "True" ]; then
-	# Remove if kitchen sink course folder exists
-	if [[ -d /tmp/ks_source ]]; then
-	  sudo rm -fr /tmp/ks_source
-	fi
+# If EDXAPP_IMPORT_KITCHENSINK_COURSE then import Kitchen Sink Course
 
-	# Create folder for kitchen sink course
-	sudo mkdir /tmp/ks_source
-	cd /tmp
+pushd /tmp
 
-	# Download kitchen sink course from github to folder /tmp/ks_source 
-	sudo git clone https://github.com/Microsoft/oxa_kitchen_sink.git ks_source
-
-	sudo chown -R edxapp:www-data /tmp/ks_source
-
-	# Go to edx-platform folder for importing
-	cd /edx/app/edxapp/edx-platform/
-
-	# Import kitchen sink course into the platform
-	sudo -u www-data /edx/bin/python.edxapp ./manage.py cms --settings=aws import /edx/var/edxapp/data /tmp/ks_source
+# Remove if kitchen sink course folder exists
+if [[ -d ks_source ]]; then
+    sudo rm -fr ks_source
 fi
 
+# Download kitchen sink course from github to folder /tmp/ks_source 
+sudo git clone https://github.com/Microsoft/oxa_kitchen_sink.git ks_source
 
+sudo chown -R edxapp:www-data ks_source
 
+# Go to edx-platform folder for importing
+pushd /edx/app/edxapp/edx-platform/
 
+# Import kitchen sink course into the platform
+sudo -u www-data /edx/bin/python.edxapp ./manage.py cms --settings=aws import /edx/var/edxapp/data /tmp/ks_source
+
+popd
+popd
