af4695f71c129d6c37d30eda2eb11d7b442e935f
Stephen Dolenc <sdolenc@users.noreply.github.com>
2018-03-05 09:13:45 -0800

    Update keep-alive.sh

--- tests/keep-alive.sh
+++ tests/keep-alive.sh
@@ -3,7 +3,7 @@
 
 # Every nine minutes
 seconds=$((9 * 60))
-message="Ah, ha, ha, ha, stayin' alive, stayin' alive"
+message="Ah, ha, ha, ha, stayin' alive, stayin' alive!  "
 
 for (( a=1; a<=7; a++ )) ; do
     sleep $seconds
