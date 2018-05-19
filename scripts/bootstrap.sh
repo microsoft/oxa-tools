506e85f1b7a7a7ecaa63a7497fb48f890d076034
btelnes <btelnes@microsoft.com>
2018-05-18 17:46:00 -0700

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -193,13 +193,11 @@ parse_args()
             ;; 
           --edxapp-lms-allowed-hosts)
             # Convert list of domains from comma delimited string to space delimited string then to array
-            #export EDXAPP_LMS_ALLOWED_HOSTS=(${"${arg_value}"//,/ })
-            export EDXAPP_LMS_ALLOWED_HOSTS="${arg_value}"
+            export EDXAPP_LMS_ALLOWED_HOSTS=(${"${arg_value}"//,/ })
             ;;
           --edxapp-cms-allowed-hosts)
             # Convert list of domains from comma delimited string to space delimited string then to array  
-            #export EDXAPP_CMS_ALLOWED_HOSTS=(${"${arg_value}"//,/ })
-            export EDXAPP_CMS_ALLOWED_HOSTS="${arg_value}"
+            export EDXAPP_CMS_ALLOWED_HOSTS=(${"${arg_value}"//,/ })
             ;;
           *)
             # Unknown option encountered
