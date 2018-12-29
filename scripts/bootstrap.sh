45d1d8bae4cbfcbd39937ced0d95666096ad9543
Elton Carr, II <eltonc@microsoft.com>
2018-12-28 17:35:32 -0800

    Add migration execution to JB

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -445,9 +445,11 @@ update_stamp_jb()
 {
     SUBJECT="${MAIL_SUBJECT} - EdX Database (Mysql) Setup Failed"
 
-    # 1. setup the OPENEDX_RELEASE variable:
+    # 1. setup the OPENEDX_RELEASE and other variables:
     export OPENEDX_RELEASE=$EDX_PLATFORM_PUBLIC_GITHUB_PROJECTBRANCH
     export EDX_OPENEDX_RELEASE=$EDX_VERSION
+    export OXA_PLAYBOOK_CONFIGS=$OXA_PLAYBOOK_CONFIG
+    export OXA_VAULT_NAME="${CLUSTER_NAME}-kv"
 
     # 2. Bootstrap the Ansible installation:
     bash $ANSIBLE_BOOTSTRAP_INSTALLER
@@ -455,13 +457,16 @@ update_stamp_jb()
 
     # 3. Install Open edX using custom native installer
     target_playbook="edx-stateful.yml"
-
-    # switching to export since edx uses $@ to pass additional environment vars to the playbooks
     export OXA_TARGET_PLAYBOOK=$target_playbook
-    export OXA_PLAYBOOK_CONFIGS=$OXA_PLAYBOOK_CONFIG
-    export OXA_VAULT_NAME="${CLUSTER_NAME}-kv"
 
     bash $NATIVE_INSTALLER
+    exit_on_error "Execution of native installer failed (Stamp JB: ${NATIVE_INSTALLER}, ${target_playbook}, ${OXA_PLAYBOOK_CONFIG})" 1 "${SUBJECT}" "${CLUSTER_ADMIN_EMAIL}" "${PRIMARY_LOG}" "${SECONDARY_LOG}"
+
+    # 4. Run stateless for the purposes of running migrations
+    target_playbook="edx-stateless.yml"
+    export OXA_TARGET_PLAYBOOK=$target_playbook
+
+    bash $NATIVE_INSTALLER -e "migrate_db=yes" --tags "migrate"
     exit_on_error "Execution of native installer failed (Stamp JB: ${NATIVE_INSTALLER}, ${target_playbook}, ${OXA_PLAYBOOK_CONFIG})" 1 "${SUBJECT}" "${CLUSTER_ADMIN_EMAIL}" "${PRIMARY_LOG}" "${SECONDARY_LOG}"
 
     # if the Memcache Server is different than the Mysql Master server, we have to install memcache with default configs
